e82dad8ab855b99935e1ad61e408c64f83374a54
Mawaddah <151591967+flymunn@users.noreply.github.com>
2024-04-23 14:45:46 +0700

    tugas 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+use App\Http\Controllers\TodoTaskController;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+// Route::get('/', function () {
+// //     return view('home');
+// });
+// $tasks = [
+//     [
+//         'task'    => 'Task1',
+//         'tanggal' => '2022-03-21',
+//     ],
+//     [
+//         'task'    => 'Task2',
+//         'tanggal' => '2022-03-22',
+//     ]
+// ];
+Route::get('/', [TodoTaskController::class, 'index']);
+Route::post('/', [TodoTaskController::class, 'store'])->name('tasks.store');
+Route::delete('/deleteTask/{id}', [TodoTaskController::class, 'deleteTask']);
